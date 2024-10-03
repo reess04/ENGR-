42e81d4f775b0f17bc648c25e6b756522d2a51b5
reess04 <reess@oregonstate.edu>
2024-10-02 19:13:49 -0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Spencer Rees
+# GitHub username: reess04
+# Date: 10/02/2024
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input('Bear')
+print("Your favorite animal is the " + fave_animal + ".")
